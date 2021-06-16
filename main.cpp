f3a7ddb6ad7341278f365ff75b6fe8b47b15561e
Miablo <themio@themioshow.com>
2021-06-16 00:50:47 +0000

    fixed ifstream

--- main.cpp
+++ main.cpp
@@ -3,27 +3,35 @@
 #include<sstream>
 #include<vector>
 #include<fstream>
+#include<string>
 
 //--- namespace --//
 using std::endl;
 using std::cout;
 using std::cin;
+using std::string;
+using std::ifstream;
+
+//-- file input --//
+ifstream fin;
 
 //--- function prototypes --//
 void swapOh();
-
-//---- file ---//
-ifsteam fin;
 //---- Global Var ---//
 
 int main (){
 	string temp;
+	int n;
+
 	fin.open("input.txt");
 	// find first line which is number
 	if(fin.is_open()){
-		while(getline(fin,temp){
-			cout << temp << endl;
-		})
+		while(getline(fin,temp)){
+			cin << n;
+			while(!boardBuilt){
+				
+			}
+		}
 	} else {
 		cout << "Error opening file.\n";
 	}
